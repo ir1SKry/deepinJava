65199ad052ede50da77b0e8d2b4f0f288ea72ca1
wangjin18 <wangjin18@jd.com>
2017-08-24 16:33:34 +0800

    添加子数组最大和

---
+++ src/main/java/com/ustbcafe/deepin/algorithms/dp/LCSSum.java
@@ -0,0 +1,43 @@
+package com.ustbcafe.deepin.algorithms.dp;
+
+import java.util.Scanner;
+
+/**
+ * Created by wangjin18 on 2017/8/24.
+ *
+ *  Largest sum of continuous sub array
+ *  子数组的最大和
+ *  program pear
+ *
+ */
+public class LCSSum {
+
+    public static void main(String[] args){
+
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int[] array=new int[n];
+        for(int i=0;i<n;i++)
+           array[i]= in.nextInt();
+        int max=largestSum(array);
+        System.out.println("子数组的最大和："+max);
+    }
+
+    /*
+     *  sum[i] 表示以下标i结尾的子数组的最大和
+     *   leetcode
+     *   53. Maximum Subarray
+     * */
+    public static int largestSum(int[]  nums){
+        int[] sum=new int[nums.length];
+        sum[0]=nums[0];
+        int max=sum[0];
+        for(int i=1;i<nums.length;i++){
+            sum[i]=Math.max(nums[i],sum[i-1]+nums[i]);
+            max=Math.max(sum[i],max);
+        }
+        return max;
+    }
+
+
+}
--- src/main/java/com/ustbcafe/deepin/algorithms/search/Dijkstra.java
+++ src/main/java/com/ustbcafe/deepin/algorithms/search/Dijkstra.java
@@ -4,15 +4,40 @@ import java.util.Scanner;
 
 /**
  * Created by wangjin18 on 2017/8/22.
- *
- * http://ac.jobdu.com/problem.php?pid=1447 
+ * http://ac.jobdu.com/problem.php?pid=1447
+ * 单源最短路径算法
+ * http://blog.csdn.net/acm_jl/article/details/51002489
  */
 public class Dijkstra {
 
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int N=in.nextInt(); //路口数
-        int M=in.nextInt(); // 路的条数
+        while(true) {
+            int N = in.nextInt(); //路口数
+            int M = in.nextInt(); // 路的条数
+            if(N==0&&M==0)
+                break;
+            int[][] costMatrix=new int[N+1][N+1];
+            for(int i=0;i<M;i++){
+                   int row=in.nextInt();
+                   int column=in.nextInt();
+                   int cost=in.nextInt();
+                   costMatrix[row][column]=cost;
+                   costMatrix[column][row]=cost;
 
+            }
+        }
+    }
+    /*
+     * @param N 路口数
+     * @param M 路的数量
+     * @param a  起点路口
+     * @param b 终点路口
+     *
+     * */
+    public static int time(int N,int M,int a,int b){
+
+
+        return 0;
     }
 }
